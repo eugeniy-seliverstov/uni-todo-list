60ab1a9096068235cf33e304ec57a9cadcd8d2b4
Eugeniy <serdsaerrr@mail.ru>
2020-04-10 17:23:36 +0500

    add item parser

--- parser/dns-parser.js
+++ parser/dns-parser.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
-const { filename, parseUrl, stepSize, storagePath } = require('./config');
+const url = require('url');
+const { filename, siteUrl, parseUrl, stepSize, storagePath } = require('./config');
 
 const { promises: fsp } = fs;
 
@@ -20,7 +21,36 @@ const Selectors = {
 };
 
 async function parseItem(pathname) {
-    return { pathname };
+    try {
+        const response = await fetch(url.resolve(siteUrl, pathname));
+        const body = await response.text();
+        const $ = cheerio.load(body);
+        const price = parseFloat($(Selectors.price).eq(0).text().replace(/\s/g, ''));
+        const code = parseInt($(Selectors.productCode).text());
+        const name = $(Selectors.productName).text();
+        const description = $(Selectors.description).text();
+        const rows = $(Selectors.paramsRows);
+ 
+        const properties = {};
+        let currentPart = 'none';
+        rows.each((i, row) => {
+            const part = $(row).find(Selectors.tablePart).eq(0);
+ 
+            if (part.length > 0) {
+                currentPart = part.text();
+                properties[currentPart] = {};
+            } else {
+                const property = $(row).find(Selectors.paramName).text().trim();
+                const value = $(row).find(Selectors.paramValue).text().trim();
+                properties[currentPart][property] = value;
+            }
+        });
+ 
+        return { name, price, code, description, properties };
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 async function parsePage(page = 1, items = []) {
