d6eba4892cc3492bdf240d7cee874940d5b5e564
Rustam Shirinov <18350550+D9RS@users.noreply.github.com>
2020-04-12 18:57:45 +0500

    added item list and 404

---
+++ front/src/components/Error404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="error">
+    <h1>Страница не найдена (404)</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error404"
+};
+</script>
---
+++ front/src/components/ItemList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="item-list">
+    <Item
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @delete-todo="deleteTodo"
+    />
+  </div>
+</template>
+
+<script>
+import Item from "@/components/Item/Item";
+
+export default {
+  name: "ItemList",
+  props: ["todos"],
+  components: {
+    Item
+  },
+  methods: {
+    deleteTodo: function(id) {
+      this.$store.dispatch("removeTodo", id);
+    }
+  }
+};
+</script>
