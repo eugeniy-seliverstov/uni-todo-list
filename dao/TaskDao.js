d6d0a486b8dc6751271d45035bbdca7ca02aa4a2
Rustam Shirinov <18350550+D9RS@users.noreply.github.com>
2020-04-13 16:25:19 +0500

    add error handling

--- dao/TaskDao.js
+++ dao/TaskDao.js
@@ -2,23 +2,43 @@ const db = require('../core/db');
 
 class TaskDao {
     async getByUserId(userId) {
-        const [tasks] = await db.query('SELECT * FROM task WHERE user_id = ?', [userId]);
-        return tasks;
+        try {
+            const [tasks] = await db.query('SELECT * FROM task WHERE user_id = ?', [userId]);
+            return tasks;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     }
 
     async insert(text, userId) {
-        const [result] = await db.query('INSERT INTO task (text, user_id) values (?, ?)', [text, userId]);
-        return result;
+        try {
+            const [result] = await db.query('INSERT INTO task (text, user_id) values (?, ?)', [text, userId]);
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     }
 
     async update(text, isDone, id, userId) {
-        const [result] = await db.query('UPDATE task SET text = ?, is_done = ? WHERE id = ? AND user_id = ?', [text, isDone, id, userId]);
-        return result;
+        try {
+            const [result] = await db.query('UPDATE task SET text = ?, is_done = ? WHERE id = ? AND user_id = ?', [text, isDone, id, userId]);
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     }
 
     async delete(id, userId) {
-        const [result] = await db.query('DELETE FROM task WHERE id = ? AND user_id = ?', [id, userId]);
-        return result;
+        try {
+            const [result] = await db.query('DELETE FROM task WHERE id = ? AND user_id = ?', [id, userId]);
+            return result;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     }
 }
 
